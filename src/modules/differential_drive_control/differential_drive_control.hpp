b4a57468f82d1104958ef243e98556dcde611fe7
PerFrivik <pfrivik@ethz.ch>
2023-11-13 15:02:52 +0100

    Rover: Updated header formatting

--- src/modules/differential_drive_control/differential_drive_control.hpp
+++ src/modules/differential_drive_control/differential_drive_control.hpp
@@ -92,22 +92,22 @@ private:
 
 	uORB::PublicationMulti<actuator_motors_s> _outputs_pub{ORB_ID(actuator_motors)};
 
-	uORB::Subscription _control_mode_sub{ORB_ID(vehicle_control_mode)};
+	uORB::Subscription _vehicle_control_mode_sub{ORB_ID(vehicle_control_mode)};
 	uORB::Subscription _manual_control_setpoint_sub{ORB_ID(manual_control_setpoint)};
 	uORB::Subscription _parameter_update_sub{ORB_ID(parameter_update)};
 	uORB::Subscription _vehicle_status_sub{ORB_ID(vehicle_status)};
 
-	vehicle_control_mode_s			_control_mode{};
-	actuator_motors_s 			_actuator_motors{};
+	vehicle_control_mode_s _vehicle_control_mode{};
+	actuator_motors_s _actuator_motors{};
 
-	differential_drive_control_kinematics 	_differential_kinematics_controller;
+	differential_drive_control_kinematics _differential_kinematics_controller;
 
 	matrix::Vector2f _input_feed_forward{0.0f, 0.0f};  // _input_feed_forward[0] -> Vx [m/s], _input_feed_forward[1] -> Omega [rad/s]
 	matrix::Vector2f _output_inverse{0.0f, 0.0f}; // _output[0] -> Right Motor [rad/s], _output[1] -> Left Motor [rad/s]
 	matrix::Vector2f _output_forwards{0.0f, 0.0f}; // _output[0] -> Right Motor [rad/s], _output[1] -> Left Motor [rad/s]
 
-	float 		 _forwards_velocity{0.0f};
-	float 		 _angular_velocity{0.0f};
+	float _forwards_velocity{0.0f};
+	float _angular_velocity{0.0f};
 
 	float _dt{0.0};
 	float _last_timestamp{0.0};
