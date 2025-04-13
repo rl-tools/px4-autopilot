2387651623ad996cc7b5610224c53a38d9161768
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-04-12 20:14:54 -0400

    adding buttons to manual_control_setpoint to forward joystick buttons from MAVLink MANUAL_CONTROL in SITL

--- src/modules/mavlink/mavlink_receiver.cpp
+++ src/modules/mavlink/mavlink_receiver.cpp
@@ -2054,6 +2054,7 @@ MavlinkReceiver::handle_message_manual_control(mavlink_message_t *msg)
 	manual_control_setpoint.throttle = ((mavlink_manual_control.z / 1000.f) * 2.f) - 1.f;
 	manual_control_setpoint.yaw = mavlink_manual_control.r / 1000.f;
 	manual_control_setpoint.data_source = manual_control_setpoint_s::SOURCE_MAVLINK_0 + _mavlink->get_instance_id();
+	manual_control_setpoint.buttons = mavlink_manual_control.buttons;
 	manual_control_setpoint.timestamp = manual_control_setpoint.timestamp_sample = hrt_absolute_time();
 	_manual_control_input_pub.publish(manual_control_setpoint);
 }
